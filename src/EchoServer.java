32dc33e6128edbc17669e6fe79e393781fc2b64b
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-23 12:23:06 +0200

    finished making the EchoServer classes startServer method

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class EchoServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EchoServer {
+    private int port;
+    private ServerSocket ss;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void startServer() throws IOException {
+        ss = new ServerSocket(port);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        while(true){
+            System.out.println("Waiting for client");
+            Socket s = ss.accept();
+            ClientHandler ch = new ClientHandler(s);
+            executorService.execute(ch);
+        }
+    }
 }
