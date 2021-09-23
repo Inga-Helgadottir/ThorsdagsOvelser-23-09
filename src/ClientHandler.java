bacd4e46743096e06e267fbf5f5b760948394574
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-23 12:17:02 +0200

    made the protocol method with messages, a switchcase to handle user input and called the method from the classes run method

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,14 +7,49 @@ public class ClientHandler implements Runnable{
     Socket client;
     PrintWriter pw;
     Scanner sc;
+    Counter c = new Counter();
 
     public ClientHandler(Socket client) throws IOException {
         this.client = client;
         this.pw = new PrintWriter(client.getOutputStream(),true);
         this.sc = new Scanner(client.getInputStream());
     }
+
+    public void protocol() throws IOException{
+        String userInfo = "";
+        pw.println("You are connected, here are your options:");
+        pw.println("-To see the current count write SEE");
+        pw.println("-To add to the count write ADD");
+        pw.println("-To remove from the count write REMOVE");
+        pw.println("-To close program write CLOSE");
+        while (!userInfo.equals("CLOSE")){
+            userInfo = sc.nextLine().toUpperCase();
+            switch (userInfo){
+                case "ADD":
+                    c.addToCount();
+                    break;
+                case "SEE":
+                    c.getCount();
+                    break;
+                case "REMOVE":
+                    c.removeFromCount();
+                    break;
+                default:
+                    userInfo = "CLOSE";
+            }
+        }
+        client.close();
+        pw.println(userInfo + " - fra serveren");
+    }
+
     @Override
     public void run() {
-
+        while (true) {
+            try {
+                this.protocol();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
