9f555a8e11bbfc20f4a1509abd1917171481151f
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-23 14:21:01 +0200

    moved counter and added comments given to the users

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,15 +7,15 @@ public class ClientHandler implements Runnable{
     Socket client;
     PrintWriter pw;
     Scanner sc;
-    Counter c = new Counter();
 
-    public ClientHandler(Socket client) throws IOException {
+    public ClientHandler(Socket client) throws IOException, NullPointerException {
         this.client = client;
         this.pw = new PrintWriter(client.getOutputStream(),true);
         this.sc = new Scanner(client.getInputStream());
     }
 
     public void protocol() throws IOException{
+        Counter c = new Counter();
         String userInfo = "";
         pw.println("You are connected, here are your options:");
         pw.println("-To see the current count write SEE");
@@ -27,15 +27,24 @@ public class ClientHandler implements Runnable{
             switch (userInfo){
                 case "ADD":
                     c.addToCount();
+                    pw.println("Someone has entered the stadium");
+                    System.out.println(Thread.currentThread().getName() + "- someone has entered the stadium, current count -" + c.getCount());
                     break;
                 case "SEE":
                     c.getCount();
+                    pw.println("Current count - " + c.getCount());
+                    System.out.println(Thread.currentThread().getName() + " is checking out the current count" + c.getCount());
                     break;
                 case "REMOVE":
                     c.removeFromCount();
+                    pw.println("Someone has left the stadium");
+                    System.out.println(Thread.currentThread().getName() + "- someone has left the stadium, current count -" + c.getCount());
+                    break;
+                case "CLOSE":
                     break;
                 default:
-                    userInfo = "CLOSE";
+                    pw.println("Your input does not match any of our options, try again");
+                    userInfo = "";
             }
         }
         client.close();
