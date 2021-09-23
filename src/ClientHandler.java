0344c106878ff9fd4f516755df95d903978e1e58
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2021-09-23 19:35:28 +0200

    updated switch case alittle

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -7,15 +7,16 @@ public class ClientHandler implements Runnable{
     Socket client;
     PrintWriter pw;
     Scanner sc;
+    Counter c;
 
     public ClientHandler(Socket client) throws IOException, NullPointerException {
         this.client = client;
         this.pw = new PrintWriter(client.getOutputStream(),true);
         this.sc = new Scanner(client.getInputStream());
+        this.c = new Counter();
     }
 
     public void protocol() throws IOException{
-        Counter c = new Counter();
         String userInfo = "";
         pw.println("You are connected, here are your options:");
         pw.println("-To see the current count write SEE");
@@ -25,22 +26,23 @@ public class ClientHandler implements Runnable{
         while (!userInfo.equals("CLOSE")){
             userInfo = sc.nextLine().toUpperCase();
             switch (userInfo){
+                case "SEE":
+                    c.getCount();
+                    pw.println("Current count : " + c.getCount());
+                    System.out.println(Thread.currentThread().getName() + " is checking out the current count : " + c.getCount());
+                    break;
                 case "ADD":
                     c.addToCount();
                     pw.println("Someone has entered the stadium");
-                    System.out.println(Thread.currentThread().getName() + "- someone has entered the stadium, current count -" + c.getCount());
-                    break;
-                case "SEE":
-                    c.getCount();
-                    pw.println("Current count - " + c.getCount());
-                    System.out.println(Thread.currentThread().getName() + " is checking out the current count" + c.getCount());
+                    System.out.println(Thread.currentThread().getName() + " - someone has entered the stadium, current count : " + c.getCount());
                     break;
                 case "REMOVE":
                     c.removeFromCount();
                     pw.println("Someone has left the stadium");
-                    System.out.println(Thread.currentThread().getName() + "- someone has left the stadium, current count -" + c.getCount());
+                    System.out.println(Thread.currentThread().getName() + " - someone has left the stadium, current count : " + c.getCount());
                     break;
                 case "CLOSE":
+                    userInfo = "CLOSE";
                     break;
                 default:
                     pw.println("Your input does not match any of our options, try again");
